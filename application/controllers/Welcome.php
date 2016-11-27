274641d543c6675e826b8ffa86cc2382152ea5db
gurvirjohal <gurvir_johal95@hotmail.com>
2016-11-27 13:54:04 -0800

    Steps 1-7 completed Signed-off-by:gurvirjohal <gurvir_johal95@hotmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,20 +6,7 @@ class Welcome extends Application {
 
 	public function index()
 	{
-		$result = '';
-		$oddrow = true;
-		foreach ($this->categories->all() as $category)
-		{
-//			$viewparms = array(
-//				'direction' => ($oddrow ? 'left' : 'right')
-//			);
-//			$viewparms = array_merge($viewparms, $category);
-			$category->direction = ($oddrow ? 'left' : 'right');
-			$result .= $this->parser->parse('category-home', $category, true);
-			$oddrow = ! $oddrow;
-		}
-		$this->data['content'] = $result;
-		$this->render();
+		echo 'Access Denied';
 	}
 
 }
